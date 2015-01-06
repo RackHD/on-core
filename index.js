b5b0e8cf0477612067e80417ac1e4bac8e25080a
John Frey <jfrey@ndemic.com>
2015-01-06 11:06:55 -0800

    walking skeleton

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-module.exports = {};
+// Copyright 2014, Renasar Technologies Inc.
+/* jshint: node:true */
+
+'use strict';
+
+var _ = require('lodash');
+
+module.exports = function (di, directory) {
+    var helper = require('./lib/common/di.js')(di, directory || __dirname);
+
+    return {
+        helper: helper,
+        injectables: _.flatten(
+            [
+                helper.requireGlob(__dirname + '/lib/protocol/*.js'),
+                helper.requireGlob(__dirname + '/lib/services/*.js'),
+                helper.requireWrapper('q', 'Q'),
+                helper.requireWrapper('nconf'),
+                helper.simpleWrapper(_, '_'),
+                require('./lib/common/constants.js'),
+                require('./lib/common/logger.js')
+            ]
+        )
+    };
+};
