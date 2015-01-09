0c739786affa31c10002a0ace94749c4b9ee01c8
Joe Heck <heckj@mac.com>
2015-01-08 16:26:21 -0800

    porting in assert and error service tests from common

--- spec/lib/services/assert-spec.js
+++ spec/lib/services/assert-spec.js
@@ -1,8 +1,56 @@
 // Copyright 2014, Renasar Technologies Inc.
-/* jshint node:true */
+/* jshint node: true */
+/* jshint expr:true */
 
 'use strict';
 
-describe(__filename, function () {
-    it('needs specs');
+var assertPlus = require('assert-plus');
+
+describe("AssertService", function() {
+    var assert = helper.baseInjector.get('Services.Assert'),
+        config = helper.baseInjector.get('Services.Configuration');
+
+    _.methods(assertPlus).forEach(function (method) {
+        it('should have a ' + method + ' method', function () {
+            expect(assert).to.respondTo(method);
+        });
+    });
+
+    it('should work the way assert-plus intended', function () {
+        config.set('assert', 'throw');
+
+        expect(function () {
+            assert.bool('not a bool');
+        }).to.throw();
+
+        expect(function () {
+            assert.bool(true);
+        }).to.not.throw();
+
+        expect(function () {
+            assert.bool(false);
+        }).to.not.throw();
+
+        expect(function () {
+            assert.ok(undefined);
+        }).to.throw();
+    });
+
+    it('should noop if configured off', function () {
+        expect(function() {
+            config.set('assert', 'off');
+
+            assert.ok(false);
+        }).to.not.throw();
+    });
+
+    it('should log only if configured to log', function () {
+        expect(function () {
+            config.set('assert', 'log');
+
+            assert.ok(undefined);
+        }).to.not.throw();
+    });
+
+    it('should exit if configured to exit');
 });
--- spec/lib/services/errors-spec.js
+++ spec/lib/services/errors-spec.js
@@ -1,8 +1,61 @@
 // Copyright 2014, Renasar Technologies Inc.
-/* jshint node:true */
+/* jshint node: true */
+/* jshint expr:true */
 
 'use strict';
 
-describe(__filename, function () {
-    it('needs specs');
+describe("Errors", function() {
+    var Errors = helper.baseInjector.get('Services.Errors');
+
+    describe('BaseError', function () {
+        var error = new Errors.BaseError('message');
+
+        it('should be an instance of Error', function () {
+            expect(error).to.be.an.instanceof(Error);
+        });
+
+        it('should be an instance of BaseError', function () {
+            expect(error).to.be.an.instanceof(Errors.BaseError);
+        });
+
+        it('should have a name of BaseError', function () {
+            expect(error.name).to.be.equal('BaseError');
+        });
+
+        it('should have a message of message', function () {
+            expect(error.message).to.be.equal('message');
+        });
+
+        it('should provide the correct stack trace', function () {
+            expect(error.stack.split('\n')[1]).to.contain(__filename);
+        });
+    });
+
+    describe('MyError', function () {
+        var error = new Errors.MyError('message');
+
+        it('should be an instance of Error', function () {
+            expect(error).to.be.an.instanceof(Error);
+        });
+
+        it('should be an instance of BaseError', function () {
+            expect(error).to.be.an.instanceof(Errors.BaseError);
+        });
+
+        it('should be an instance of MyError', function () {
+            expect(error).to.be.an.instanceof(Errors.MyError);
+        });
+
+        it('should have a name of MyError', function () {
+            expect(error.name).to.be.equal('MyError');
+        });
+
+        it('should have a message of message', function () {
+            expect(error.message).to.be.equal('message');
+        });
+
+        it('should provide the correct stack trace', function () {
+            expect(error.stack.split('\n')[1]).to.contain(__filename);
+        });
+    });
 });
