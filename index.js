80aa49fe7cdd96e4c2f6551eb73ea7465ca576d5
John Frey <jfrey@ndemic.com>
2015-01-12 14:54:56 -0800

    add statsd service, startup logging, etc

--- index.js
+++ index.js
@@ -25,6 +25,8 @@ module.exports = function (di, directory) {
                 helper.requireWrapper('colors/safe', 'colors'),
                 helper.requireWrapper('prettyjson', 'prettyjson'),
                 helper.requireWrapper('lru-cache', 'lru-cache'),
+                helper.requireWrapper('memwatch', 'memwatch'),
+                helper.requireWrapper('node-statsd', 'node-statsd'),
 
                 // Glob Requirables
                 helper.requireGlob(__dirname + '/lib/common/*.js'),
