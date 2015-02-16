493e53e0cb348857e3bbd61eda783c4a691f3fe1
Jason Gardino <jason.gardino@renasar.com>
2015-02-16 15:49:34 -0800

    changing "grunt watch" to run coverage

--- gruntfile.js
+++ gruntfile.js
@@ -85,7 +85,7 @@ function prepareGrunt(grunt) {
                 // grunt watch:mochaTest
                 mochaTest: {
                     files: allFiles,
-                    tasks: ['mochaTest:test'],
+                    tasks: ['coverage'],
                     options:{
                         interrupt: true,
                         atBegin: true
