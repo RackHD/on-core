e67b086b5884f8bf40973f1b1a90a18afcad6406
Joe Heck <heckj@mac.com>
2015-02-23 17:27:13 -0800

    wrap logging in Message/Result structure
     - added subscription for log as well
     - tests for verifying send through log

--- spec/lib/protocol/logging-spec.js
+++ spec/lib/protocol/logging-spec.js
@@ -7,22 +7,29 @@ describe("Logging protocol functions", function () {
     helper.before();
 
     before(function () {
-        this.logging =helper.injector.get('Protocol.Logging');
+        this.logging = helper.injector.get('Protocol.Logging');
     });
 
     helper.after();
 
-    describe("publish log message", function() {
+    describe("publish log message", function () {
 
-        it("should throw an error if log level isn't known", function() {
+        var testSubscription;
+        afterEach("cancel afterEach", function() {
+            // unsubscribe to clean up after ourselves
+            if (testSubscription) {
+                return testSubscription.dispose();
+            }
+        });
+
+        it("should throw an error if log level isn't known", function () {
             var self = this,
-                data = { foo: 'bar' };
+                data = {foo: 'bar'};
 
-            expect(function() {
+            expect(function () {
                 self.logging.publishLog('foo', data);
             }).to.throw(Error);
             //NOTE: heckj: no good way with AssertPlus to verify basic subclass of error
-
         });
 
         [
@@ -36,16 +43,34 @@ describe("Logging protocol functions", function () {
             'debug',
             'silly'
         ].forEach(function (level) {
-                it("should publish a poller alert event with level "+level, function() {
+                it("should publish a poller alert event with level " + level, function () {
                     //NOTE: no matching internal code to listen for these events
                     var self = this,
-                        data = { foo: 'bar' };
+                        Q = helper.injector.get('Q'),
+                        deferred = Q.defer(),
+                        data = {foo: 'bar'};
 
-                    return self.logging.publishLog(level, data);
+                    self.logging.subscribeLog(level, function(_data) {
+                        try {
+                            expect(_data).to.deep.equal(data);
+                            deferred.resolve();
+                        } catch(err) {
+                            deferred.reject(err);
+                        }
+                    }).then(function(subscription) {
+                        expect(subscription).to.be.ok;
+                        testSubscription = subscription;
+
+                        return self.logging.publishLog(level, data);
+                    }).catch(function(err) {
+                        deferred.reject(err);
+                    });
+
+                    return deferred.promise;
                 });
 
             });
 
-            });
+    });
 
 });
