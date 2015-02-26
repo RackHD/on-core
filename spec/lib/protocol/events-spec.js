617f4e268c79705927e7642cdf73a455c699208e
Joe Heck <heckj@mac.com>
2015-02-26 10:48:14 -0800

    protocol unit tests

--- spec/lib/protocol/events-spec.js
+++ spec/lib/protocol/events-spec.js
@@ -9,11 +9,215 @@ describe("Event protocol subscribers", function () {
 
     before(function () {
         this.events = helper.injector.get('Protocol.Events');
-        this.task = helper.injector.get('Protocol.Task');
     });
 
     helper.after();
 
-    it("needs tests");
+    describe("publish/subscribe TftpSuccess", function () {
+
+        var testSubscription;
+        afterEach("TftpSuccess afterEach", function () {
+            // unsubscribe to clean up after ourselves
+            if (testSubscription) {
+                return testSubscription.dispose();
+            }
+        });
+
+        it("should publish and subscribe to TftpSuccess messages", function (done) {
+            //NOTE: no matching internal code to listen for these events
+            var self = this,
+                nodeId = "507f191e810c19729de860ea", //mongoId format
+                data = {foo: 'bar'};
+
+            self.events.subscribeTftpSuccess(nodeId, function (_data) {
+                try {
+                    expect(_data).to.deep.equal(data);
+                    done();
+                } catch (err) {
+                    done(err);
+                }
+            }).then(function (subscription) {
+                expect(subscription).to.be.ok;
+                testSubscription = subscription;
+
+                return self.events.publishTftpSuccess(nodeId, data);
+            }).catch(function (err) {
+                done(err);
+            });
+        });
+    });
+
+    describe("publish/subscribe TftpFailure", function () {
+
+        var testSubscription;
+        afterEach("TftpFailure afterEach", function () {
+            // unsubscribe to clean up after ourselves
+            if (testSubscription) {
+                return testSubscription.dispose();
+            }
+        });
+
+        it("should publish and subscribe to TftpFailure messages", function (done) {
+            //NOTE: no matching internal code to listen for these events
+            var self = this,
+                nodeId = "507f191e810c19729de860ea", //mongoId format
+                data = {foo: 'bar'};
+
+            self.events.subscribeTftpFailure(nodeId, function (_data) {
+                try {
+                    expect(_data).to.deep.equal(data);
+                    done();
+                } catch (err) {
+                    done(err);
+                }
+            }).then(function (subscription) {
+                expect(subscription).to.be.ok;
+                testSubscription = subscription;
+
+                return self.events.publishTftpFailure(nodeId, data);
+            }).catch(function (err) {
+                done(err);
+            });
+        });
+    });
+
+    describe("publish/subscribe HttpResponse", function () {
+
+        var testSubscription;
+        afterEach("HttpResponse afterEach", function () {
+            // unsubscribe to clean up after ourselves
+            if (testSubscription) {
+                return testSubscription.dispose();
+            }
+        });
+
+        it("should publish and subscribe to HttpResponse messages", function (done) {
+            //NOTE: no matching internal code to listen for these events
+            var self = this,
+                nodeId = "507f191e810c19729de860ea", //mongoId format
+                data = {foo: 'bar'};
+
+            self.events.subscribeHttpResponse(nodeId, function (_data) {
+                try {
+                    expect(_data).to.deep.equal(data);
+                    done();
+                } catch (err) {
+                    done(err);
+                }
+            }).then(function (subscription) {
+                expect(subscription).to.be.ok;
+                testSubscription = subscription;
+
+                return self.events.publishHttpResponse(nodeId, data);
+            }).catch(function (err) {
+                done(err);
+            });
+        });
+    });
+
+    describe("publish/subscribe DhcpBoundLease", function () {
+
+        var testSubscription;
+        afterEach("DhcpBoundLease afterEach", function () {
+            // unsubscribe to clean up after ourselves
+            if (testSubscription) {
+                return testSubscription.dispose();
+            }
+        });
+
+        it("should publish and subscribe to DhcpBoundLease messages", function (done) {
+            //NOTE: no matching internal code to listen for these events
+            var self = this,
+                nodeId = "507f191e810c19729de860ea", //mongoId format
+                data = {foo: 'bar'};
+
+            self.events.subscribeDhcpBoundLease(nodeId, function (_data) {
+                try {
+                    expect(_data).to.deep.equal(data);
+                    done();
+                } catch (err) {
+                    done(err);
+                }
+            }).then(function (subscription) {
+                expect(subscription).to.be.ok;
+                testSubscription = subscription;
+
+                return self.events.publishDhcpBoundLease(nodeId, data);
+            }).catch(function (err) {
+                done(err);
+            });
+        });
+    });
+
+    describe("publish/subscribe TaskFinished", function () {
+
+        var testSubscription;
+        afterEach("DhcpBoundLease afterEach", function () {
+            // unsubscribe to clean up after ourselves
+            if (testSubscription) {
+                return testSubscription.dispose();
+            }
+        });
+
+        it("should publish and subscribe to TaskFinished messages", function (done) {
+            //NOTE: no matching internal code to listen for these events
+            var self = this,
+                uuid = helper.injector.get('uuid'),
+                taskId = uuid.v4(),
+                data = {foo: 'bar'};
+
+            self.events.subscribeTaskFinished(taskId, function (_data) {
+                try {
+                    expect(_data).to.deep.equal(data);
+                    done();
+                } catch (err) {
+                    done(err);
+                }
+            }).then(function (subscription) {
+                expect(subscription).to.be.ok;
+                testSubscription = subscription;
+
+                return self.events.publishTaskFinished(taskId, data);
+            }).catch(function (err) {
+                done(err);
+            });
+        });
+    });
+
+    describe("publish/subscribe GraphFinished", function () {
+
+        var testSubscription;
+        afterEach("GraphFinished afterEach", function () {
+            // unsubscribe to clean up after ourselves
+            if (testSubscription) {
+                return testSubscription.dispose();
+            }
+        });
+
+        it("should publish and subscribe to GraphFinished messages", function (done) {
+            //NOTE: no matching internal code to listen for these events
+            var self = this,
+                uuid = helper.injector.get('uuid'),
+                graphId = uuid.v4(),
+                status = 'testStatus';
+
+            self.events.subscribeGraphFinished(graphId, function (_data) {
+                try {
+                    expect(_data).to.deep.equal(status);
+                    done();
+                } catch (err) {
+                    done(err);
+                }
+            }).then(function (subscription) {
+                expect(subscription).to.be.ok;
+                testSubscription = subscription;
+
+                return self.events.publishGraphFinished(graphId, status);
+            }).catch(function (err) {
+                done(err);
+            });
+        });
+    });
+
 
 });
