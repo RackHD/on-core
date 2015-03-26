7a511be0310bfb42345d810899f0b61645ccf4ab
John Frey <john.frey@emc.com>
2015-03-26 12:40:35 -0700

    add statsd timing to messenger request/response
    * add Timer class which wraps up process.hrtime
    * remove memwatch from statsd for node 12 compat
    * add sanitize method to statsd for normalizing keys
    * update testing to reflect and ensure delivery of data to statsd service

---
+++ spec/lib/common/timer-spec.js
@@ -0,0 +1,56 @@
+// Copyright 2014, Renasar Technologies Inc.
+/* jshint node:true */
+
+'use strict';
+
+describe('Extensions', function () {
+    var Timer;
+
+    helper.before();
+
+    before(function () {
+        Timer = helper.injector.get('Timer');
+    });
+
+    helper.after();
+
+    describe('Timer', function () {
+        describe('start', function () {
+            it('should store hrtime internally', function () {
+                var subject = new Timer();
+
+                subject.start();
+
+                subject.time.should.be.ok;
+            });
+        });
+
+        describe('clear', function () {
+            it('should clear the time internally', function () {
+                var subject = new Timer();
+
+                subject.start();
+
+                subject.time.should.be.ok;
+
+                subject.clear();
+
+                expect(subject.time).to.be.undefined;
+            });
+        });
+
+        describe('stop', function () {
+            it('should diff the start time with the stop time', function () {
+                var subject = new Timer();
+
+                subject.start();
+
+                var diff = subject.stop();
+
+                diff.should.be.a('number');
+
+                diff.should.be.gt(0.0);
+            });
+        });
+    });
+});
--- spec/lib/services/statsd-spec.js
+++ spec/lib/services/statsd-spec.js
@@ -3,75 +3,71 @@
 
 'use strict';
 
+var dgram = require('dgram');
+
 describe("StatsD Service", function () {
+    helper.before();
 
-    helper.before(function(context) {
-        var util = require('util');
-        var events = require('events');
-
-        // Fake memwatch to drive code & events
-        var FakeMemWatch = function() {};
-        util.inherits(FakeMemWatch, events.EventEmitter);
-
-        // available as this.fakeMemWatch from within tests
-        context.fakeMemWatch = new FakeMemWatch();
-
-        return [
-            helper.di.simpleWrapper(context.fakeMemWatch, 'memwatch'),
-            helper.require('/spec/mocks/logger.js')
-        ];
+    before(function () {
+        this.subject = helper.injector.get('Services.StatsD');
     });
 
     helper.after();
 
-    describe("handleLeak", function() {
-        var loggerSpy;
-        before("handleLeak before", function() {
-            var logger = helper.injector.get("Logger").initialize();
-            loggerSpy = sinon.stub(logger, 'log');
-        });
-        beforeEach("handleLeak beforeEach", function() {
-            loggerSpy.reset();
-        });
-        after("handleLeak after", function() {
-            loggerSpy.restore();
-        });
-        it("should log info", function() {
-            var statsdService = helper.injector.get('Services.StatsD');
-            var testInfo = { foo: 'bar' };
-            statsdService.handleLeak(testInfo);
-            expect(loggerSpy).to.have.been.calledWith("warning", "memwatch leak", testInfo);
-        });
-    });
-    describe("handleStats", function() {
-        var gaugeSpy;
-        var statsdService;
-        before("handleStats before", function() {
-            statsdService = helper.injector.get('Services.StatsD');
-            gaugeSpy = sinon.spy(statsdService, "gauge");
-        });
-        beforeEach("handleStats beforeEach", function() {
-            gaugeSpy.reset();
-        });
-        after("handleStats after", function() {
-            gaugeSpy.restore();
-        });
-
-        it("should invoke statsD guage on receiving stats", function() {
-            var testStats = {
-                estimated_base : 1, //jshint ignore:line
-                current_base : 2, //jshint ignore:line
-                min : 0,
-                max : 5,
-                usage_trend : 3  //jshint ignore:line
-            };
-            statsdService.handleStats(testStats);
-            expect(gaugeSpy.getCall(0)).to.have.been.calledWith("memwatch.estimated_base", 1);
-            expect(gaugeSpy.getCall(1)).to.have.been.calledWith("memwatch.current_base", 2);
-            expect(gaugeSpy.getCall(2)).to.have.been.calledWith("memwatch.min", 0);
-            expect(gaugeSpy.getCall(3)).to.have.been.calledWith("memwatch.max", 5);
-            expect(gaugeSpy.getCall(4)).to.have.been.calledWith("memwatch.usage_trend", 3);
+    describe('StatsD Methods', function () {
+        [
+            'timing',
+            'increment',
+            'decrement',
+            'histogram',
+            'gauge',
+            'set',
+            'unique'
+        ].forEach(function (method) {
+            it('should have a ' + method, function () {
+                this.subject.should.respondTo(method);
+            });
         });
     });
 
+    describe('sanitize', function () {
+        it('should not alter a basic key', function () {
+            this.subject.sanitize('test').should.equal('test');
+        });
+
+        it('should remove bson id from the end of the key', function () {
+            this.subject.sanitize('test.551375513754f63fa62abc47').should.equal('test');
+        });
+
+        it('should remove uuidv4 from the end of the key', function () {
+            this.subject.sanitize('test.de305d54-75b4-431b-adb2-eb6b9e546013').should.equal('test');
+        });
+    });
+
+    describe('Publishing', function () {
+        before(function () {
+            var deferred = Q.defer();
+
+            this.server = dgram.createSocket('udp4');
+            this.server.bind(8125);
+
+            this.server.once('listening', function () {
+                deferred.resolve();
+            });
+
+            return deferred.promise;
+        });
+
+        it('should publish metrics to localhost:8125', function (done) {
+            this.server.once('message', function () {
+                done();
+            });
+
+            this.subject.gauge('test', 1);
+        });
+
+        after(function () {
+            this.server.close();
+        });
+    });
 });
