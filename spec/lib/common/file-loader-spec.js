78d040119abdfb01ecae1744412827069e682cd9
Ben Broderick Phillips <ben.broderickphillips@emc.com>
2015-03-30 17:32:22 -0700

    Refactor and update lookup tests, minor test fixes/updates

--- spec/lib/common/file-loader-spec.js
+++ spec/lib/common/file-loader-spec.js
@@ -5,30 +5,39 @@
 
 describe('FileLoader', function () {
     var FileLoader;
+    var fs;
 
     helper.before();
 
     before(function () {
         FileLoader = helper.injector.get('FileLoader');
+        fs = helper.injector.get('fs');
 
-        this.fs = helper.injector.get('fs');
         this.subject = new FileLoader();
+
+        sinon.stub(fs, 'writeFile');
+        sinon.stub(fs, 'readFile');
+        sinon.stub(fs, 'readdir');
+    });
+
+    beforeEach(function() {
+        fs.writeFile.reset();
+        fs.readFile.reset();
+        fs.readdir.reset();
     });
 
     helper.after(function () {
-        this.fs.writeFile.restore();
-        this.fs.readFile.restore();
-        this.fs.readdir.restore();
+        fs.writeFile.restore();
+        fs.readFile.restore();
+        fs.readdir.restore();
     });
 
     describe('put', function () {
         it('should write the contents to the specified file', function () {
-            var self = this;
-
-            self.fs.writeFile = sinon.stub().yields(undefined, 'put');
+            fs.writeFile.yields(undefined, 'put');
 
             return this.subject.put('filename', 'contents').then(function (contents) {
-                self.fs.writeFile.should.have.been.calledWith('filename', 'contents');
+                fs.writeFile.should.have.been.calledWith('filename', 'contents');
 
                 contents.should.equal('put');
             });
@@ -37,12 +46,10 @@ describe('FileLoader', function () {
 
     describe('get', function () {
         it('should get the contents for the specified file', function () {
-            var self = this;
-
-            self.fs.readFile = sinon.stub().yields(undefined, 'get');
+            fs.readFile.yields(undefined, 'get');
 
             return this.subject.get('filename').then(function (contents) {
-                self.fs.writeFile.should.have.been.calledWith('filename');
+                fs.readFile.should.have.been.calledWith('filename');
 
                 contents.should.equal('get');
             });
@@ -53,13 +60,11 @@ describe('FileLoader', function () {
         it(
             'should return a promise fulfilled with the file basename to contents in an object',
             function () {
-                var self = this;
-
-                self.fs.readFile = sinon.stub().yields(undefined, 'getAll');
-                self.fs.readdir = sinon.stub().yields(undefined, ['/tmp/foo.txt']);
+                fs.readFile.yields(undefined, 'getAll');
+                fs.readdir.yields(undefined, ['/tmp/foo.txt']);
 
                 return this.subject.getAll('/tmp').then(function (files) {
-                    self.fs.readdir.should.have.been.calledWith('/tmp');
+                    fs.readdir.should.have.been.calledWith('/tmp');
 
                     files['foo.txt'].should.equal('getAll');
                 });
--- spec/lib/services/lookup-spec.js
+++ spec/lib/services/lookup-spec.js
@@ -4,16 +4,28 @@
 'use strict';
 
 describe('Lookup Service', function () {
-    var lookupService, dhcpProtocol, MacAddress, Errors, waterline;
+    var lookupService, leaseCache, MacAddress, Errors, waterline;
 
     helper.before();
 
     before("Lookup Service before", function () {
         lookupService = helper.injector.get('Services.Lookup');
         waterline = helper.injector.get('Services.Waterline');
-        dhcpProtocol = helper.injector.get('Protocol.Dhcp');
+        leaseCache = helper.injector.get('DhcpLeaseCache');
         MacAddress = helper.injector.get('MacAddress');
         Errors = helper.injector.get('Errors');
+
+        sinon.stub(waterline.nodes, 'findByIdentifier');
+        sinon.stub(leaseCache, 'getLeaseByIp');
+    });
+
+    beforeEach("Lookup Service beforeEach", function() {
+        waterline.nodes.findByIdentifier.reset();
+        leaseCache.getLeaseByIp.reset();
+
+        // reset internal caches
+        lookupService._macToNodeIdCache.reset();
+        lookupService._ipAddressToMacAddressCache.reset();
     });
 
     afterEach("Lookup Service afterEach", function () {
@@ -22,108 +34,58 @@ describe('Lookup Service', function () {
         }
     });
 
-    helper.after();
+    helper.after(function() {
+        waterline.nodes.findByIdentifier.restore();
+        leaseCache.getLeaseByIp.restore();
+    });
 
     describe('macAddressToNodeId', function () {
-        var macAddressToNodeStub;
-
         before('macAddressToNodeId before', function () {
-            macAddressToNodeStub = sinon.stub(lookupService, 'macAddressToNode');
+            sinon.stub(lookupService, 'macAddressToNode');
         });
 
-        beforeEach('macAddressToNodeId beforeEach', function () {
-            macAddressToNodeStub.reset();
+        beforeEach('macAddressToNodeId beforeEach', function() {
+            lookupService.macAddressToNode.reset();
         });
 
         after('macAddressToNodeId after', function () {
-            macAddressToNodeStub.restore();
+            lookupService.macAddressToNode.restore();
         });
 
         it('macAddressToNodeId should return a LookupError if there is no node', function () {
-            macAddressToNodeStub.resolves(null);
+            lookupService.macAddressToNode.resolves(null);
 
             return lookupService.macAddressToNodeId('08:00:27:7a:c0:00').should.be.rejectedWith(
-                Errors.LookupError,
-                'Unable to locate node via MAC address.');
+                Errors.LookupError, 'Unable to locate node via MAC address.');
         });
     });
-    describe("macAddressToNode", function () {
-        var findByIdentifierStub;
-        var testNode = {
-            id: '123456',
-            name: 'testNode'
-        };
-        before("macAddressToNode before", function () {
-            findByIdentifierStub = sinon.stub(waterline.nodes, 'findByIdentifier')
-                .returns(Q.resolve(testNode));
-        });
-        beforeEach("macAddressToNode beforeEach", function () {
-            findByIdentifierStub.reset();
-        });
-        after("macAddressToNode after", function () {
-            findByIdentifierStub.restore();
-        });
+
+    describe('macAddressToNode', function() {
         it("should look up a node using the macaddress", function () {
+            var testNode = {
+                id: '123456',
+                name: 'testNode'
+            };
+            waterline.nodes.findByIdentifier.resolves(testNode);
             return lookupService.macAddressToNode('08:00:27:7a:c0:00')
                 .then(function (node) {
                     expect(node).to.deep.equal(testNode);
                 });
         });
-    });
-    describe("ipAddressToMacAddress", function () {
-        var lookupIpLeaseStub;
-        before("ipAddressToMacAddress before", function () {
-            lookupIpLeaseStub = sinon.stub(dhcpProtocol, 'lookupIpLease')
-                .returns(Q.resolve('11:22:33:44:55:66'));
-        });
-        beforeEach("ipAddressToMacAddress beforeEach", function () {
-            lookupIpLeaseStub.reset();
-            lookupService._ipAddressToMacAddressCache.reset();
-        });
-        after("ipAddressToMacAddress after", function () {
-            lookupIpLeaseStub.restore();
-        });
 
-        it("should lookup a macaddress from an IP address", function () {
-            return lookupService.ipAddressToMacAddress('1.2.3.4')
-                .then(function (macaddress) {
-                    expect(macaddress).to.equal('11:22:33:44:55:66');
-                    expect(lookupIpLeaseStub).to.have.been.calledWith('1.2.3.4');
-                });
-        });
-        it("should attempt cache lookup macaddress before using protocol", function () {
-            //pre set up the cache
-            lookupService._ipAddressToMacAddressCache.set('9.8.7.6', 'aa:bb:cc:11:22:33');
-            return lookupService.ipAddressToMacAddress('9.8.7.6')
-                .then(function (macaddress) {
-                    expect(macaddress).to.equal('aa:bb:cc:11:22:33');
-                    expect(lookupIpLeaseStub).to.not.have.been.called;
-                });
-        });
-    });
-    describe("macAddressToNodeId", function () {
-        var findByIdentifierStub;
-        before("macAddressToNodeId before", function () {
-            findByIdentifierStub = sinon.stub(waterline.nodes, 'findByIdentifier');
-        });
-        beforeEach("macAddressToNodeId beforeEach", function () {
-            findByIdentifierStub.reset();
-            lookupService._macToNodeIdCache.reset();
-        });
-        after("macAddressToNodeId after", function () {
-            findByIdentifierStub.restore();
-        });
         it("should look up a node using the macaddress", function () {
-            findByIdentifierStub.returns(Q.resolve({
+            waterline.nodes.findByIdentifier.resolves({
                 id: '123456',
                 name: 'testNode'
-            }));
+            });
             return lookupService.macAddressToNodeId('08:00:27:7a:c0:00')
                 .then(function (nodeId) {
                     expect(nodeId).to.equal('123456');
-                    expect(findByIdentifierStub).to.have.been.calledWith('08:00:27:7a:c0:00');
+                    expect(waterline.nodes.findByIdentifier)
+                        .to.have.been.calledWith('08:00:27:7a:c0:00');
                 });
         });
+
         it("should use internal cache over calling waterline", function () {
             var testNodeId = '123456789';
             var testMacAddr = '44:55:66:aa:bb:cc';
@@ -132,29 +94,36 @@ describe('Lookup Service', function () {
             return lookupService.macAddressToNodeId(testMacAddr)
                 .then(function (nodeId) {
                     expect(nodeId).to.equal(testNodeId);
-                    expect(findByIdentifierStub).to.not.have.been.called;
+                    expect(waterline.nodes.findByIdentifier).to.not.have.been.called;
+                });
+        });
+    });
+
+    describe("ipAddressToMacAddress", function () {
+        it("should lookup a macaddress from an IP address", function () {
+            leaseCache.getLeaseByIp.resolves({
+                macAddress: '11:22:33:44:55:66',
+                ip: '1.2.3.4'
+            });
+            return lookupService.ipAddressToMacAddress('1.2.3.4')
+                .then(function (macaddress) {
+                    expect(macaddress).to.equal('11:22:33:44:55:66');
+                    expect(leaseCache.getLeaseByIp).to.have.been.calledWith('1.2.3.4');
+                });
+        });
+
+        it("should attempt cache lookup macaddress before using protocol", function () {
+            //pre set up the cache
+            lookupService._ipAddressToMacAddressCache.set('9.8.7.6', 'aa:bb:cc:11:22:33');
+            return lookupService.ipAddressToMacAddress('9.8.7.6')
+                .then(function (macaddress) {
+                    expect(macaddress).to.equal('aa:bb:cc:11:22:33');
+                    expect(leaseCache.getLeaseByIp).to.not.have.been.called;
                 });
         });
     });
 
     describe("ipAddressToNode", function () {
-        var findByIdentifierStub;
-        var lookupIpLeaseStub;
-        before("ipAddressToNode before", function () {
-            findByIdentifierStub = sinon.stub(waterline.nodes, 'findByIdentifier');
-            lookupIpLeaseStub = sinon.stub(dhcpProtocol, 'lookupIpLease');
-        });
-        beforeEach("ipAddressToNode beforeEach", function () {
-            findByIdentifierStub.reset();
-            lookupIpLeaseStub.reset();
-            // reset internal caches
-            lookupService._macToNodeIdCache.reset();
-            lookupService._ipAddressToMacAddressCache.reset();
-        });
-        after("ipAddressToNode after", function () {
-            findByIdentifierStub.restore();
-            lookupIpLeaseStub.restore();
-        });
         it('should look up an ip address to a node', function () {
             var testIP = "11.23.13.14";
             var testMac = "de:ad:be:ef:11:22";
@@ -162,36 +131,22 @@ describe('Lookup Service', function () {
                 id: '123456',
                 name: 'testNode'
             };
-            lookupIpLeaseStub.returns(Q.resolve(testMac));
-            findByIdentifierStub.returns(Q.resolve(testNode));
+            leaseCache.getLeaseByIp.resolves({
+                macAddress: testMac,
+                ip: testIP
+            });
+            waterline.nodes.findByIdentifier.resolves(testNode);
 
             return lookupService.ipAddressToNode(testIP)
                 .then(function (node) {
                     expect(node).to.deep.equal(testNode);
-                    expect(lookupIpLeaseStub).to.have.been.calledWith(testIP);
-                    expect(findByIdentifierStub).to.have.been.calledWith(testMac);
+                    expect(leaseCache.getLeaseByIp).to.have.been.calledWith(testIP);
+                    expect(waterline.nodes.findByIdentifier).to.have.been.calledWith(testMac);
                 });
         });
     });
 
     describe("ipAddressToNodeId", function () {
-        var findByIdentifierStub;
-        var lookupIpLeaseStub;
-        before("ipAddressToNodeId before", function () {
-            findByIdentifierStub = sinon.stub(waterline.nodes, 'findByIdentifier');
-            lookupIpLeaseStub = sinon.stub(dhcpProtocol, 'lookupIpLease');
-        });
-        beforeEach("ipAddressToNodeId beforeEach", function () {
-            findByIdentifierStub.reset();
-            lookupIpLeaseStub.reset();
-            // reset internal caches
-            lookupService._macToNodeIdCache.reset();
-            lookupService._ipAddressToMacAddressCache.reset();
-        });
-        after("ipAddressToNodeId after", function () {
-            findByIdentifierStub.restore();
-            lookupIpLeaseStub.restore();
-        });
         it('should look up an ip address to a node ID', function () {
             var testIP = "11.23.13.14";
             var testMac = "de:ad:be:ef:11:22";
@@ -199,52 +154,45 @@ describe('Lookup Service', function () {
                 id: '123456',
                 name: 'testNode'
             };
-            lookupIpLeaseStub.returns(Q.resolve(testMac));
-            findByIdentifierStub.returns(Q.resolve(testNode));
+            leaseCache.getLeaseByIp.resolves({
+                macAddress: testMac,
+                ip: testIP
+            });
+            waterline.nodes.findByIdentifier.resolves(testNode);
 
             return lookupService.ipAddressToNodeId(testIP)
                 .then(function (nodeId) {
                     expect(nodeId).to.equal('123456');
-                    expect(lookupIpLeaseStub).to.have.been.calledWith(testIP);
-                    expect(findByIdentifierStub).to.have.been.calledWith(testMac);
+                    expect(leaseCache.getLeaseByIp).to.have.been.calledWith(testIP);
+                    expect(waterline.nodes.findByIdentifier).to.have.been.calledWith(testMac);
                 });
         });
         it('should return undefined if the node is unknown', function () {
-            var testIP = "11.23.13.14";
             var testMac = "de:ad:be:ef:11:22";
-            lookupIpLeaseStub.returns(Q.resolve(testMac));
-            findByIdentifierStub.returns(Q.resolve(null));
+            var testIP = "11.23.13.14";
+            leaseCache.getLeaseByIp.resolves({
+                macAddress: testMac,
+                ip: testIP
+            });
+            waterline.nodes.findByIdentifier.resolves(null);
 
             return lookupService.ipAddressToNodeId(testIP)
                 .then(function (nodeId) {
                     expect(nodeId).to.be.undefined;
-                    expect(lookupIpLeaseStub).to.have.been.calledWith(testIP);
-                    expect(findByIdentifierStub).to.have.been.calledWith(testMac);
+                    expect(leaseCache.getLeaseByIp).to.have.been.calledWith(testIP);
+                    expect(waterline.nodes.findByIdentifier).to.have.been.calledWith(testMac);
                 });
         });
     });
 
     describe("ipAddressToMacAddressMiddleware", function () {
-        var lookupIpLeaseStub;
-        before("ipAddressToMacAddressMiddleware before", function () {
-            lookupIpLeaseStub = sinon.stub(dhcpProtocol, 'lookupIpLease');
-
-        });
-        beforeEach("ipAddressToMacAddressMiddleware beforeEach", function () {
-            lookupIpLeaseStub.reset();
-            // reset internal caches
-            lookupService._macToNodeIdCache.reset();
-            lookupService._ipAddressToMacAddressCache.reset();
-
-        });
-        after("ipAddressToMacAddressMiddleware after", function () {
-            lookupIpLeaseStub.restore();
-        });
         it("should annotate macaddress onto the request using req.ip", function (done) {
             var middleware = lookupService.ipAddressToMacAddressMiddleware();
 
             var testMac = '11:22:33:44:55:66';
-            lookupIpLeaseStub.returns(Q.resolve(testMac));
+            leaseCache.getLeaseByIp.resolves({
+                macAddress: testMac
+            });
 
             var req = {
                 ip: '2.3.4.5'
@@ -258,10 +206,13 @@ describe('Lookup Service', function () {
 
             middleware(req, res, next);
         });
+
         it("should annotate macaddress onto the request using req._remoteAddress", function (done) {
             var middleware = lookupService.ipAddressToMacAddressMiddleware();
             var testMac = '11:22:33:44:55:66';
-            lookupIpLeaseStub.returns(Q.resolve(testMac));
+            leaseCache.getLeaseByIp.resolves({
+                macAddress: testMac
+            });
             var req = {
                 _remoteAddress: '2.3.4.5'
             };
@@ -273,10 +224,13 @@ describe('Lookup Service', function () {
             };
             middleware(req, res, next);
         });
+
         it("should annotate macaddress onto the request using req.connection", function (done) {
             var middleware = lookupService.ipAddressToMacAddressMiddleware();
             var testMac = '11:22:33:44:55:66';
-            lookupIpLeaseStub.returns(Q.resolve(testMac));
+            leaseCache.getLeaseByIp.resolves({
+                macAddress: testMac
+            });
             var req = {
                 connection: {
                     remoteAddress: '2.3.4.5'
@@ -291,6 +245,7 @@ describe('Lookup Service', function () {
             };
             middleware(req, res, next);
         });
+
         it("should throw error is IP address can't be found on request", function() {
             var middleware = lookupService.ipAddressToMacAddressMiddleware();
 
