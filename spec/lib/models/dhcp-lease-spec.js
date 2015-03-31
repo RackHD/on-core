e3e28e32545aa3e3937d2f01e4babed67cdd857a
Ben Broderick Phillips <ben.broderickphillips@emc.com>
2015-03-30 17:32:42 -0700

    Add model spec for dhcp-lease

---
+++ spec/lib/models/dhcp-lease-spec.js
@@ -0,0 +1,55 @@
+// Copyright 2014, Renasar Technologies Inc.
+/* jshint node: true */
+
+'use strict';
+
+var base = require('./base-spec');
+
+describe('Models.DhcpLease', function() {
+    helper.before();
+
+    base.before(function(context) {
+        context.model = helper.injector.get('Services.Waterline').dhcpleases;
+        context.attributes = context.model._attributes;
+    });
+
+    helper.after();
+
+    describe('Base', function() {
+        base.examples();
+    });
+
+    describe('Attributes', function () {
+        describe('macAddress', function () {
+            before(function () {
+                this.subject = this.attributes.macAddress;
+            });
+
+            it('should be a string', function () {
+                expect(this.subject.type).to.equal('string');
+            });
+
+            it('should be required', function () {
+                expect(this.subject.required).to.equal(true);
+            });
+
+            it('should be unique', function () {
+                expect(this.subject.unique).to.equal(true);
+            });
+        });
+
+        describe('ip', function () {
+            before(function () {
+                this.subject = this.attributes.ip;
+            });
+
+            it('should be a string', function () {
+                expect(this.subject.type).to.equal('string');
+            });
+
+            it('should be required', function () {
+                expect(this.subject.required).to.equal(true);
+            });
+        });
+    });
+});
