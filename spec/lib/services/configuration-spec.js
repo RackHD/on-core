1f7249c4ebcd0233325db4324d3692aabce77218
John Frey <john.frey@emc.com>
2015-04-16 06:59:40 -0700

    add test coverage for configuration service

--- spec/lib/services/configuration-spec.js
+++ spec/lib/services/configuration-spec.js
@@ -4,5 +4,41 @@
 'use strict';
 
 describe(require('path').basename(__filename), function () {
-    it('needs specs');
+    helper.before();
+
+    before(function () {
+        this.subject = helper.injector.get('Services.Configuration');
+    });
+
+    helper.after();
+
+    describe('Instance Methods', function () {
+
+        describe('set', function() {
+            it('should chain', function() {
+                this.subject.set('foo', 'bar').should.equal(this.subject);
+            });
+
+            it('should set the key to the given value', function() {
+                this.subject.set('foo', 'bar').get('foo').should.equal('bar');
+            });
+        });
+
+        describe('get', function() {
+            it('should return the requested value', function() {
+                this.subject.set('foo', 'bar').get('foo').should.equal('bar');
+            });
+
+            it('should use the default value provided if no value is defined', function() {
+                this.subject.get('missing', 'override').should.be.equal('override');
+            });
+        });
+
+        describe('getAll', function() {
+            it('should return all configuration values', function() {
+                this.subject.getAll().should.be.an('object');
+            });
+        });
+    });
 });
+
