68bfe62166bd38a4ea4aee9396b8ce35703c8bb4
Tracy Gius <tracy.gius@emc.com>
2016-01-05 13:35:24 -0800

    fixed build number for core

--- build_children.sh
+++ build_children.sh
@@ -8,7 +8,7 @@ set -x
 # Get last child project build number
 BUILD_NUM_DHCP=$(curl -s 'https://api.travis-ci.org/repos/RackHD/on-dhcp-proxy/builds' | grep -o '^\[{"id":[0-9]*,' | grep -o '[0-9]' | tr -d '\n')
 BUILD_NUM_HTTP=$(curl -s 'https://api.travis-ci.org/repos/RackHD/on-http/builds' | grep -o '^\[{"id":[0-9]*,' | grep -o '[0-9]' | tr -d '\n')
-BUILD_NUM_HTTP=$(curl -s 'https://api.travis-ci.org/repos/RackHD/on-core/builds' | grep -o '^\[{"id":[0-9]*,' | grep -o '[0-9]' | tr -d '\n')
+BUILD_NUM_CORE=$(curl -s 'https://api.travis-ci.org/repos/RackHD/on-core/builds' | grep -o '^\[{"id":[0-9]*,' | grep -o '[0-9]' | tr -d '\n')
 
 # Restart last child project build
 curl -X POST https://api.travis-ci.org/builds/$BUILD_NUM_DHCP/restart --header "Authorization: token "$AUTH_TOKEN_DHCP
