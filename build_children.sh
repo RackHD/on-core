2680b359ebe5c0959590668c42fbeeaf3df2d7bf
Tracy Gius <tracy.gius@emc.com>
2016-01-11 13:06:04 -0800

    adding fake auth token for testing

--- build_children.sh
+++ build_children.sh
@@ -5,6 +5,8 @@
 set -e
 set -x
 
+echo $AUTH_TOKEN
+
 # Get last child project build number
 BUILD_NUM_DHCP=$(curl -s 'https://api.travis-ci.org/repos/RackHD/on-dhcp-proxy/builds' | grep -o '^\[{"id":[0-9]*,' | grep -o '[0-9]' | tr -d '\n')
 BUILD_NUM_HTTP=$(curl -s 'https://api.travis-ci.org/repos/RackHD/on-http/builds' | grep -o '^\[{"id":[0-9]*,' | grep -o '[0-9]' | tr -d '\n')
