31ea8bcd8b855f3dd6821cca854bf2771e5c9ef3
Tracy Gius <tracy.gius@emc.com>
2016-01-12 13:26:17 -0800

    trying secure environment

--- build_children.sh
+++ build_children.sh
@@ -12,9 +12,14 @@ fi
 # Get last child project build number
 BUILD_NUM_DHCP=$(curl -s 'https://api.travis-ci.org/repos/RackHD/on-dhcp-proxy/builds' | grep -o '^\[{"id":[0-9]*,' | grep -o '[0-9]' | tr -d '\n')
 BUILD_NUM_HTTP=$(curl -s 'https://api.travis-ci.org/repos/RackHD/on-http/builds' | grep -o '^\[{"id":[0-9]*,' | grep -o '[0-9]' | tr -d '\n')
-BUILD_NUM_CORE=$(curl -s 'https://api.travis-ci.org/repos/RackHD/on-core/builds' | grep -o '^\[{"id":[0-9]*,' | grep -o '[0-9]' | tr -d '\n')
+BUILD_NUM_TFP=$(curl -s 'https://api.travis-ci.org/repos/RackHD/on-core/builds' | grep -o '^\[{"id":[0-9]*,' | grep -o '[0-9]' | tr -d '\n')
+BUILD_NUM_TASKS=$(curl -s 'https://api.travis-ci.org/repos/RackHD/on-core/builds' | grep -o '^\[{"id":[0-9]*,' | grep -o '[0-9]' | tr -d '\n')
+
 
 # Restart last child project build
 curl -X POST https://api.travis-ci.org/builds/$BUILD_NUM_DHCP/restart --header "Authorization: token "$AUTH_TOKEN
 curl -X POST https://api.travis-ci.org/builds/$BUILD_NUM_HTTP/restart --header "Authorization: token "$AUTH_TOKEN
-curl -X POST https://api.travis-ci.org/builds/$BUILD_NUM_CORE/restart --header "Authorization: token "$AUTH_TOKEN
+curl -X POST https://api.travis-ci.org/builds/$BUILD_NUM_TFTP/restart --header "Authorization: token "$AUTH_TOKEN
+curl -X POST https://api.travis-ci.org/builds/$BUILD_NUM_TASKS/restart --header "Authorization: token "$AUTH_TOKEN
+
+
