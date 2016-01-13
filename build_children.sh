e8229628cf6275075e6de6a1db69866fc0b8cc4f
Tracy Gius <tracy.gius@emc.com>
2016-01-13 15:24:21 -0800

    moved on-http as an on-tasks depended

--- build_children.sh
+++ build_children.sh
@@ -10,21 +10,21 @@ GetMasterBuild () {
 
 # Get last child project build number
 BUILD_NUM_DHCP=$(GetMasterBuild on-dhcp-proxy)
-BUILD_NUM_HTTP=$(GetMasterBuild on-http)
 BUILD_NUM_TASKS=$(GetMasterBuild on-tasks)
 BUILD_NUM_TFTP=$(GetMasterBuild on-tftp)
 BUILD_NUM_SYSLOG=$(GetMasterBuild on-syslog)
 
 # Restart last child project build
 curl -X POST https://api.travis-ci.org/builds/$BUILD_NUM_DHCP/restart --header "Authorization: token "$AUTH_TOKEN
-curl -X POST https://api.travis-ci.org/builds/$BUILD_NUM_HTTP/restart --header "Authorization: token "$AUTH_TOKEN
 curl -X POST https://api.travis-ci.org/builds/$BUILD_NUM_TASKS/restart --header "Authorization: token "$AUTH_TOKEN
 curl -X POST https://api.travis-ci.org/builds/$BUILD_NUM_TFTP/restart --header "Authorization: token "$AUTH_TOKEN
 curl -X POST https://api.travis-ci.org/builds/$BUILD_NUM_SYSLOG/restart --header "Authorization: token "$AUTH_TOKEN
 
 #Echo out what builds where restarted
 echo Restarted on-dhcp-proxy Build = $BUILD_NUM_DHCP
-echo Restarted on-http Build = $BUILD_NUM_HTTP
 echo Restarted on-tasks = $BUILD_NUM_TASKS
 echo Restarted on-tftp Build = $BUILD_NUM_TFTP
 echo Restarted on-syslog Build = $BUILD_NUM_SYSLOG
+
+
+echo "Note - on-core restarts builds for on-dhcp-proxy, on-tftp, on-syslog, and on-tasks. When on-tasks succeeds, it restarts builds for on-taskgraph and on-http. "
