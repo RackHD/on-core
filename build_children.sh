ccedb2e484e6487e7acdf4c025d1b038e5ef462f
Tracy Gius <tracy.gius@emc.com>
2016-01-13 14:34:58 -0800

    only builds the most recent master build, no pr builds

--- build_children.sh
+++ build_children.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+#Gets the latest Master build
+GetMasterBuild () {
+
+    BUILD=$(curl -s 'https://api.travis-ci.org/repos/RackHD/'$1'/branches/master' | grep -o '{"branch":{"id":[0-9]*,' | grep -o '[0-9]' | tr -d '\n')
+
+    echo $BUILD
+}
+
 # Get last child project build number
-BUILD_NUM_DHCP=$(curl -s 'https://api.travis-ci.org/repos/RackHD/on-dhcp-proxy/builds' | grep -o '^\[{"id":[0-9]*,' | grep -o '[0-9]' | tr -d '\n')
-BUILD_NUM_HTTP=$(curl -s 'https://api.travis-ci.org/repos/RackHD/on-http/builds' | grep -o '^\[{"id":[0-9]*,' | grep -o '[0-9]' | tr -d '\n')
-BUILD_NUM_TASKS=$(curl -s 'https://api.travis-ci.org/repos/RackHD/on-tasks/builds' | grep -o '^\[{"id":[0-9]*,' | grep -o '[0-9]' | tr -d '\n')
-BUILD_NUM_TFTP=$(curl -s 'https://api.travis-ci.org/repos/RackHD/on-tftp/builds' | grep -o '^\[{"id":[0-9]*,' | grep -o '[0-9]' | tr -d '\n')
-BUILD_NUM_SYSLOG=$(curl -s 'https://api.travis-ci.org/repos/RackHD/on-syslog/builds' | grep -o '^\[{"id":[0-9]*,' | grep -o '[0-9]' | tr -d '\n')
+BUILD_NUM_DHCP=$(GetMasterBuild on-dhcp-proxy)
+BUILD_NUM_HTTP=$(GetMasterBuild on-http)
+BUILD_NUM_TASKS=$(GetMasterBuild on-tasks)
+BUILD_NUM_TFTP=$(GetMasterBuild on-tftp)
+BUILD_NUM_SYSLOG=$(GetMasterBuild on-syslog)
 
 # Restart last child project build
 curl -X POST https://api.travis-ci.org/builds/$BUILD_NUM_DHCP/restart --header "Authorization: token "$AUTH_TOKEN
@@ -14,3 +22,9 @@ curl -X POST https://api.travis-ci.org/builds/$BUILD_NUM_TASKS/restart --header 
 curl -X POST https://api.travis-ci.org/builds/$BUILD_NUM_TFTP/restart --header "Authorization: token "$AUTH_TOKEN
 curl -X POST https://api.travis-ci.org/builds/$BUILD_NUM_SYSLOG/restart --header "Authorization: token "$AUTH_TOKEN
 
+#Echo out what builds where restarted
+echo Restarted on-dhcp-proxy Build = $BUILD_NUM_DHCP
+echo Restarted on-http Build = $BUILD_NUM_HTTP
+echo Restarted on-tasks = $BUILD_NUM_TASKS
+echo Restarted on-tftp Build = $BUILD_NUM_TFTP
+echo Restarted on-syslog Build = $BUILD_NUM_SYSLOG
