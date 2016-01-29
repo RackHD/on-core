63cb4b6b3cf3afe617bac5edc8a4c441a8aee8a5
Joseph Longever <joseph.longever@emc.com>
2016-01-29 12:41:43 -0500

    Refactor dhcp-spec unit-test - Remove rabbitmq-server service dependency.

--- spec/lib/protocol/dhcp-spec.js
+++ spec/lib/protocol/dhcp-spec.js
@@ -1,39 +1,51 @@
 // Copyright 2015, EMC, Inc.
 
-
 'use strict';
 
 describe("DHCP protocol functions", function () {
-    var testSubscription;
-
+    var testSubscription,
+        testMessage,
+        testIP = '1.1.1.1',
+        testMac = "00:11:22:aa:bb:dd",
+        sampleError = new Error('someError'),
+        messenger,
+        dhcp;
+    
     helper.before();
 
     before(function () {
-        this.dhcp = helper.injector.get('Protocol.Dhcp');
+        dhcp = helper.injector.get('Protocol.Dhcp');        
+        messenger = helper.injector.get('Services.Messenger');
+        var Message = helper.injector.get('Message');
+        var Subscription = helper.injector.get('Subscription');
+        
+        testSubscription = new Subscription({},{});
+        testMessage = new Message({},{},{});
+        sinon.stub(testMessage);
+        sinon.stub(messenger);
     });
-
-    afterEach(function () {
-        if (testSubscription) {
-            return testSubscription.dispose();
-        }
+    
+    beforeEach(function() {
+        messenger.subscribe.reset();
+        messenger.request.reset();
     });
-
+ 
     helper.after();
 
     describe("lookupIpLease", function () {
         it("should subscribe and receive lookupIpLease results", function () {
-            var self = this,
-                testIP = "1.1.1.1",
-                testResult = {mac: '00:11:22:aa:bb:cc'};
-
-            return self.dhcp.subscribeLookupIpLease(function (_data) {
+            var testResult = {mac: '00:11:22:aa:bb:cc'};
+            messenger.subscribe = sinon.spy(function(a,b,callback,d) {
+                callback({value:testIP},testMessage);
+                return Promise.resolve(testSubscription);
+            });
+            messenger.request.resolves({value:testResult.mac});
+            return dhcp.subscribeLookupIpLease(function (_data) {
                 expect(_data).to.equal(testIP);
                 return testResult;
             }).then(function (subscription) {
                 expect(subscription).to.be.ok;
-
-                testSubscription = subscription;
-                return self.dhcp.lookupIpLease(testIP);
+                return dhcp.lookupIpLease(testIP);
             }).then(function (data) {
                 expect(data).to.equal("00:11:22:aa:bb:cc");
             });
@@ -41,301 +53,242 @@ describe("DHCP protocol functions", function () {
 
         it("should subscribe and receive empty lookupIpLease result if there's no value",
             function () {
-                var self = this,
-                    testIP = "1.1.1.1";
-
-                return self.dhcp.subscribeLookupIpLease(function (_data) {
+                messenger.request.resolves(Promise.resolve({value:{}}));
+                return dhcp.subscribeLookupIpLease(function (_data) {
                     expect(_data).to.equal(testIP);
                     return;
                 }).then(function (subscription) {
                     expect(subscription).to.be.ok;
-
-                    testSubscription = subscription;
-                    return self.dhcp.lookupIpLease(testIP);
+                    return dhcp.lookupIpLease(testIP);
                 }).then(function (data) {
                     expect(data).to.deep.equal({});
                 });
             });
 
         it("should subscribe and receive lookupIpLease failures", function () {
-            var self = this,
-                testIP = "1.1.1.1",
-                sampleError = new Error('someError');
-
-            var ErrorEvent = helper.injector.get('ErrorEvent');
-
-            return self.dhcp.subscribeLookupIpLease(function (_data) {
+            messenger.request.rejects(sampleError);
+            return dhcp.subscribeLookupIpLease(function (_data) {
                 expect(_data).to.equal(testIP);
                 throw sampleError;
             }).then(function (subscription) {
                 expect(subscription).to.be.ok;
-
-                testSubscription = subscription;
-                return self.dhcp.lookupIpLease(testIP);
-            }).should.be.rejectedWith(ErrorEvent, 'someError');
+                return dhcp.lookupIpLease(testIP);
+            }).should.be.rejectedWith(sampleError);
         });
     });
 
     describe("pinMac", function () {
         it("should subscribe and receive pinMac results", function () {
-            var self = this,
-                testMac = "00:11:22:aa:bb:dd",
-                testResult = true;
-
-            return self.dhcp.subscribePinMac(function (_data) {
+            var testResult = true;
+            messenger.subscribe = sinon.spy(function(a,b,callback,d) {
+                callback({value:testMac},testMessage);
+                return Promise.resolve(testSubscription);
+            });
+            messenger.request.resolves(Promise.resolve({value:testResult}));
+            return dhcp.subscribePinMac(function (_data) {
                 expect(_data).to.equal(testMac);
                 return testResult;
             }).then(function (subscription) {
                 expect(subscription).to.be.ok;
-
-                testSubscription = subscription;
-                return self.dhcp.pinMac(testMac);
+                return dhcp.pinMac(testMac);
             }).then(function (data) {
                 expect(data).to.equal(testResult);
             });
         });
 
         it("should subscribe and receive pinMac failures", function () {
-            var self = this,
-                testMac = "00:11:22:aa:bb:dd",
-                sampleError = new Error('someError');
-
-            var ErrorEvent = helper.injector.get('ErrorEvent');
-
-            return self.dhcp.subscribePinMac(function (_data) {
+            messenger.request.rejects(sampleError);
+            return dhcp.subscribePinMac(function (_data) {
                 expect(_data).to.equal(testMac);
                 throw sampleError;
             }).then(function (subscription) {
                 expect(subscription).to.be.ok;
-
-                testSubscription = subscription;
-                return self.dhcp.pinMac(testMac);
-            }).should.be.rejectedWith(ErrorEvent, 'someError');
+                return dhcp.pinMac(testMac);
+            }).should.be.rejectedWith(sampleError);
         });
     });
 
     describe("unpinMac", function () {
         it("should subscribe and receive unpinMac results", function () {
-            var self = this,
-                testMac = "00:11:22:aa:bb:ee",
-                testResult = true;
-
-            return self.dhcp.subscribeUnpinMac(function (_data) {
+            var testResult = true;
+            messenger.subscribe = sinon.spy(function(a,b,callback,d) {
+                callback({value:testMac},testMessage);
+                return Promise.resolve(testSubscription);
+            });
+            messenger.request.resolves(Promise.resolve({value:testResult}));
+            return dhcp.subscribeUnpinMac(function (_data) {
                 expect(_data).to.equal(testMac);
                 return testResult;
             }).then(function (subscription) {
                 expect(subscription).to.be.ok;
-
-                testSubscription = subscription;
-                return self.dhcp.unpinMac(testMac);
+                return dhcp.unpinMac(testMac);
             }).then(function (data) {
                 expect(data).to.equal(testResult);
             });
         });
 
         it("should subscribe and receive unpinMac failures", function () {
-            var self = this,
-                testMac = "00:11:22:aa:bb:ee",
-                sampleError = new Error('someError');
-
-            var ErrorEvent = helper.injector.get('ErrorEvent');
-
-            return self.dhcp.subscribeUnpinMac(function (_data) {
+            messenger.request.rejects(sampleError);
+            return dhcp.subscribeUnpinMac(function (_data) {
                 expect(_data).to.equal(testMac);
                 throw sampleError;
             }).then(function (subscription) {
                 expect(subscription).to.be.ok;
-
-                testSubscription = subscription;
-                return self.dhcp.unpinMac(testMac);
-            }).should.be.rejectedWith(ErrorEvent, 'someError');
+                return dhcp.unpinMac(testMac);
+            }).should.be.rejectedWith(sampleError);
         });
     });
 
     describe("unpinIp", function () {
         it("should subscribe and receive unpinIp results", function () {
-            var self = this,
-                testIp = "4.3.2.1",
-                testResult = true;
-
-            return self.dhcp.subscribeUnpinIp(function (_data) {
-                expect(_data).to.equal(testIp);
+            var testResult = true;
+            messenger.subscribe = sinon.spy(function(a,b,callback,d) {
+                callback({value:testIP},testMessage);
+                return Promise.resolve(testSubscription);
+            });
+            messenger.request.resolves(Promise.resolve({value:testResult}));
+            return dhcp.subscribeUnpinIp(function (_data) {
+                expect(_data).to.equal(testIP);
                 return testResult;
             }).then(function (subscription) {
                 expect(subscription).to.be.ok;
-
-                testSubscription = subscription;
-                return self.dhcp.unpinIp(testIp);
+                return dhcp.unpinIp(testIP);
             }).then(function (data) {
                 expect(data).to.equal(testResult);
             });
         });
 
         it("should subscribe and receive unpinIp failures", function () {
-            var self = this,
-                testIp = "4.3.2.1",
-                sampleError = new Error('someError');
-
-            var ErrorEvent = helper.injector.get('ErrorEvent');
-
-            return self.dhcp.subscribeUnpinIp(function (_data) {
-                expect(_data).to.equal(testIp);
+            messenger.request.rejects(sampleError);
+            return dhcp.subscribeUnpinIp(function (_data) {
+                expect(_data).to.equal(testIP);
                 throw sampleError;
             }).then(function (subscription) {
                 expect(subscription).to.be.ok;
-
-                testSubscription = subscription;
-                return self.dhcp.unpinIp(testIp);
-            }).should.be.rejectedWith(ErrorEvent, 'someError');
+                return dhcp.unpinIp(testIP);
+            }).should.be.rejectedWith(sampleError);
         });
     });
 
     describe("ipInRange", function () {
         it("should subscribe and receive ipInRange results", function () {
-            var self = this,
-                testIp = "4.3.2.1",
-                testResult = true;
-
-            return self.dhcp.subscribeIpInRange(function (_data) {
-                expect(_data).to.equal(testIp);
+            var testResult = true;
+            messenger.subscribe = sinon.spy(function(a,b,callback,d) {
+                callback({value:testIP},testMessage);
+                return Promise.resolve(testSubscription);
+            });
+            messenger.request.resolves(Promise.resolve({value:testResult}));
+            return dhcp.subscribeIpInRange(function (_data) {
+                expect(_data).to.equal(testIP);
                 return testResult;
             }).then(function (subscription) {
                 expect(subscription).to.be.ok;
-
-                testSubscription = subscription;
-                return self.dhcp.ipInRange(testIp);
+                return dhcp.ipInRange(testIP);
             }).then(function (data) {
                 expect(data).to.equal(testResult);
             });
         });
 
         it("should subscribe and receive ipInRange failures", function () {
-            var self = this,
-                testIp = "4.3.2.1",
-                sampleError = new Error('someError');
-
-            var ErrorEvent = helper.injector.get('ErrorEvent');
-
-            return self.dhcp.subscribeIpInRange(function (_data) {
-                expect(_data).to.equal(testIp);
+            messenger.request.rejects(sampleError);
+            return dhcp.subscribeIpInRange(function (_data) {
+                expect(_data).to.equal(testIP);
                 throw sampleError;
             }).then(function (subscription) {
                 expect(subscription).to.be.ok;
-
-                testSubscription = subscription;
-                return self.dhcp.ipInRange(testIp);
-            }).should.be.rejectedWith(ErrorEvent, 'someError');
+                return dhcp.ipInRange(testIP);
+            }).should.be.rejectedWith(sampleError);
         });
     });
 
     describe("peekLeaseTable", function () {
         it("should subscribe and receive peekLeaseTable results", function () {
-            var self = this,
-                testResult = {leases: [1, 2, 3]};
-
-            return self.dhcp.subscribePeekLeaseTable(function () {
+            var testResult = {leases: [1, 2, 3]};
+            messenger.subscribe = sinon.spy(function(a,b,callback,d) {
+                callback({value:testResult},testMessage);
+                return Promise.resolve(testSubscription);
+            });
+            messenger.request.resolves(Promise.resolve({value:testResult}));
+            return dhcp.subscribePeekLeaseTable(function () {
                 return testResult;
             }).then(function (subscription) {
                 expect(subscription).to.be.ok;
-
-                testSubscription = subscription;
-                return self.dhcp.peekLeaseTable();
+                return dhcp.peekLeaseTable();
             }).then(function (data) {
                 expect(data).to.deep.equal(testResult);
             });
         });
 
         it("should subscribe and receive peekLeaseTable failures", function () {
-            var self = this,
-                sampleError = new Error('someError');
-
-            var ErrorEvent = helper.injector.get('ErrorEvent');
-
-            return self.dhcp.subscribePeekLeaseTable(function () {
+            messenger.request.rejects(sampleError);
+            return dhcp.subscribePeekLeaseTable(function () {
                 throw sampleError;
             }).then(function (subscription) {
                 expect(subscription).to.be.ok;
-
-                testSubscription = subscription;
-                return self.dhcp.peekLeaseTable();
-            }).should.be.rejectedWith(ErrorEvent, 'someError');
+                return dhcp.peekLeaseTable();
+            }).should.be.rejectedWith(sampleError);
         });
     });
 
     describe("removeLease", function () {
         it("should subscribe and receive removeLease results", function () {
-            var self = this,
-                testMac = "00:11:22:aa:bb:ee",
-                testResult = true;
-
-            return self.dhcp.subscribeRemoveLease(function (_data) {
+            var testResult = true;
+            messenger.subscribe = sinon.spy(function(a,b,callback,d) {
+                callback({value:testMac},testMessage);
+                return Promise.resolve(testSubscription);
+            });
+            messenger.request.resolves(Promise.resolve({value:testResult}));
+            return dhcp.subscribeRemoveLease(function (_data) {
                 expect(_data).to.equal(testMac);
                 return testResult;
             }).then(function (subscription) {
                 expect(subscription).to.be.ok;
-
-                testSubscription = subscription;
-                return self.dhcp.removeLease(testMac);
+                return dhcp.removeLease(testMac);
             }).then(function (data) {
                 expect(data).to.equal(testResult);
             });
         });
 
         it("should subscribe and receive removeLease failures", function () {
-            var self = this,
-                testMac = "00:11:22:aa:bb:ee",
-                sampleError = new Error('someError');
-
-            var ErrorEvent = helper.injector.get('ErrorEvent');
-
-            return self.dhcp.subscribeRemoveLease(function (_data) {
+            messenger.request.rejects(sampleError);
+            return dhcp.subscribeRemoveLease(function (_data) {
                 expect(_data).to.equal(testMac);
                 throw sampleError;
             }).then(function (subscription) {
                 expect(subscription).to.be.ok;
-
-                testSubscription = subscription;
-                return self.dhcp.removeLease(testMac);
-            }).should.be.rejectedWith(ErrorEvent, 'someError');
+                return dhcp.removeLease(testMac);
+            }).should.be.rejectedWith(sampleError);
         });
     });
 
     describe("removeLeaseByIp", function () {
         it("should subscribe and receive removeLeaseByIp results", function () {
-            var self = this,
-                testIp = "4.3.2.1",
-                testResult = true;
-
-            return self.dhcp.subscribeRemoveLeaseByIp(function (_data) {
-                expect(_data).to.equal(testIp);
+            var testResult = true;
+            messenger.subscribe = sinon.spy(function(a,b,callback,d) {
+                callback({value:testIP},testMessage);
+                return Promise.resolve(testSubscription);
+            });
+            messenger.request.resolves(Promise.resolve({value:testResult}));
+            return dhcp.subscribeRemoveLeaseByIp(function (_data) {
+                expect(_data).to.equal(testIP);
                 return testResult;
             }).then(function (subscription) {
                 expect(subscription).to.be.ok;
-
-                testSubscription = subscription;
-                return self.dhcp.removeLeaseByIp(testIp);
+                return dhcp.removeLeaseByIp(testIP);
             }).then(function (data) {
                 expect(data).to.equal(testResult);
             });
         });
 
         it("should subscribe and receive removeLeaseByIp failures", function () {
-            var self = this,
-                testIp = "4.3.2.1",
-                sampleError = new Error('someError');
-
-            var ErrorEvent = helper.injector.get('ErrorEvent');
-
-            return self.dhcp.subscribeRemoveLeaseByIp(function (_data) {
-                expect(_data).to.equal(testIp);
+            messenger.request.rejects(sampleError);
+            return dhcp.subscribeRemoveLeaseByIp(function (_data) {
+                expect(_data).to.equal(testIP);
                 throw sampleError;
             }).then(function (subscription) {
                 expect(subscription).to.be.ok;
-
-                testSubscription = subscription;
-                return self.dhcp.removeLeaseByIp(testIp);
-            }).should.be.rejectedWith(ErrorEvent, 'someError');
+                return dhcp.removeLeaseByIp(testIP);
+            }).should.be.rejectedWith(sampleError);
         });
     });
-
 });
