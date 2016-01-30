6f339018fa332e197b1a4d12180f34470dc02272
Joseph Longever <joseph.longever@emc.com>
2016-01-29 20:24:41 -0500

    Refactor on-core unit-tests - Remaining protocols tests - Remove rabbitmq-server dependency.

--- spec/lib/protocol/waterline-spec.js
+++ spec/lib/protocol/waterline-spec.js
@@ -4,26 +4,36 @@
 'use strict';
 
 describe('Protocol.Waterline', function() {
-    var waterlineProtocol;
-    var collection = { identity: 'dummy' };
-
+    var waterlineProtocol,
+        collection = { identity: 'dummy' },
+        testSubscription,
+        messenger;
+        
     helper.before();
 
     before(function () {
         waterlineProtocol = helper.injector.get('Protocol.Waterline');
+        messenger = helper.injector.get('Services.Messenger');
+        var Subscription = helper.injector.get('Subscription');
+        testSubscription = new Subscription({},{});
+        sinon.stub(testSubscription);
+        sinon.stub(messenger);
     });
 
     helper.after();
 
     it('should publish a created event', function() {
+        messenger.publish.resolves();
         return waterlineProtocol.publishRecord(collection, 'created', { id: 1 });
     });
 
     it('should publish an updated event', function() {
+        messenger.publish.resolves();
         return waterlineProtocol.publishRecord(collection, 'updated', { id: 1 });
     });
 
-    it('should publish a destroyed event', function() {
+    it('should publish a destroyed event', function() {     
+        messenger.publish.resolves();
         return waterlineProtocol.publishRecord(collection, 'destroyed', { id: 1 });
     });
 });
