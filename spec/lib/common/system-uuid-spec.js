e76b6ce8fc9ed1479431a4bc38312d5ae209bde0
Longever, Joseph <Joseph.Longever@emc.com>
2016-04-28 12:31:30 -0400

    Add module to read local system unique identifier

---
+++ spec/lib/common/system-uuid-spec.js
@@ -0,0 +1,41 @@
+// Copyright 2016, EMC, Inc.
+'use strict';
+
+describe("SystemUuid", function() {
+    var subject;
+    var regEx = /^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$/;
+    var fs;
+    
+    before(function () {
+        helper.setupInjector();
+        subject = helper.injector.get('SystemUuid');
+        fs = helper.injector.get('fs');
+        sinon.stub(fs, 'readFileAsync');
+    });
+
+    helper.after(function() {
+        fs.readFileAsync.restore();
+    });
+    
+    beforeEach(function() {
+        fs.readFileAsync.reset();
+    });
+
+    describe('getUuid', function () {
+        it('should return system uuid', function () {
+            fs.readFileAsync.resolves('xyz');
+            return subject.getUuid()
+            .then(function(data) {
+                expect(data).to.equal('xyz');
+            });
+        });
+        
+        it('should return generated uuid', function () {
+            fs.readFileAsync.resolves(null);
+            return subject.getUuid()
+            .then(function(data) {
+                expect(regEx.test(data)).to.be.ok;
+            });
+        });
+    });
+});
