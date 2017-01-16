b9c1a14c5de8c861e70d1eda69acc41e885e2413
krein peng <zuiliang.peng@emc.com>
2017-01-16 13:14:34 +0000

    Update review comments
     * Update review comments:
        1. Add negative tests for hook publish method
        2. Use nock.cleanAll() for nock unit test
        3. Use done(new Error) instead of throw(new Error)

--- spec/lib/common/hook-spec.js
+++ spec/lib/common/hook-spec.js
@@ -30,44 +30,46 @@ describe('Hook', function () {
     afterEach("After hook test", function(){
         _publishSpy.reset();
         findStub.reset();
+        nock.cleanAll();
     });
 
-    it('should post data to hook url with http service', function () {
-        var scope = nock('http://172.1.1.1:8080')
-                    .filteringRequestBody(function(body){
-                        if (!_.isEqual(payload, JSON.parse(body))) {
-                            throw new Error('body is not matched');
-                        }
-                    })
-                    .post('/test')
-                    .reply(201, 'OK');
+    it('should post data to hook url with http service', function (done) {
+        nock('http://172.1.1.1:8080')
+            .filteringRequestBody(function(body){
+                if (!_.isEqual(payload, JSON.parse(body))) {
+                    done(new Error('body is not matched'));
+                }
+            })
+            .post('/test')
+            .reply(201, 'OK');
         findStub.withArgs({}).resolves([hooks[0]]);
         return hook.publish(payload)
         .then(function(){
             expect(hook._publish).to.have.been.calledOnce;
             expect(hook._publish).to.have.been.calledWith(hooks[0], payload);
             expect(waterline.hooks.find).to.have.been.calledOnce;
-            nock.removeInterceptor(scope);
+            done();
         });
     });
 
-    it('should post data to hook urls', function () {
-        var scopeHttps = nock('https://172.1.1.0:8080')
-                    .filteringRequestBody(function(body){
-                        if (!_.isEqual(payload, JSON.parse(body))) {
-                            throw new Error('body is not matched');
-                        }
-                    })
-                    .post('/test')
-                    .reply(201, 'OK');
-        var scopeHttp = nock('http://172.1.1.1:8080')
-                    .filteringRequestBody(function(body){
-                        if (!_.isEqual(payload, JSON.parse(body))) {
-                            throw new Error('body is not matched');
-                        }
-                    })
-                    .post('/test')
-                    .reply(201, 'OK');
+    it('should post data to hook urls', function (done) {
+        nock('https://172.1.1.0:8080')
+            .filteringRequestBody(function(body){
+                if (!_.isEqual(payload, JSON.parse(body))) {
+                    done(new Error('body is not matched'));
+                }
+            })
+            .post('/test')
+            .reply(201, 'OK');
+        nock('http://172.1.1.1:8080')
+            .filteringRequestBody(function(body){
+                if (!_.isEqual(payload, JSON.parse(body))) {
+                    done(new Error('body is not matched'));
+                }
+            })
+            .post('/test')
+            .reply(201, 'OK');
+
         findStub.withArgs({}).resolves(hooks);
         return hook.publish(payload)
         .then(function(){
@@ -75,8 +77,7 @@ describe('Hook', function () {
             expect(hook._publish).to.have.been.calledWith(hooks[0], payload);
             expect(hook._publish).to.have.been.calledWith(hooks[1], payload);
             expect(waterline.hooks.find).to.have.been.calledOnce;
-            nock.removeInterceptor(scopeHttps);
-            nock.removeInterceptor(scopeHttp);
+            done();
         });
     });
 
@@ -85,6 +86,33 @@ describe('Hook', function () {
         return hook.publish(payload)
         .then(function(){
             expect(hook._publish).to.have.not.been.called;
+        });
+    });
+
+    it('should post data to hook urls independently', function (done) {
+        nock('https://172.1.1.0:8080')
+            .filteringRequestBody(function(body){
+                if (!_.isEqual(payload, JSON.parse(body))) {
+                    done(new Error('body is not matched'));
+                }
+            })
+            .post('/test')
+            .reply(201, 'OK');
+        findStub.withArgs({}).resolves(hooks);
+        _publishSpy.restore();
+        sinon.stub(hook, '_publish').withArgs(hooks[0], payload)
+            .rejects(new Error('Failed to send data to all hooks'));
+        return hook.publish(payload)
+        .then(function(){
+            done(new Error('Test should fail'));
+        })
+        .catch(function(err){
+            expect(hook._publish).to.have.been.calledTwice;
+            expect(hook._publish).to.have.been.calledWith(hooks[0], payload);
+            expect(hook._publish).to.have.been.calledWith(hooks[1], payload);
+            expect(err.message).equals('Failed to send data to all hooks');
+            expect(waterline.hooks.find).to.have.been.calledOnce;
+            done();
         });
     });
 
