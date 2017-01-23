5e5216e6d4826db58188dd3f5c81d88a48ed055a
krein peng <zuiliang.peng@emc.com>
2017-01-23 11:43:02 +0000

    Add hook publish to publishExternalEvent method
     *  Hook feature should be called together with external event as a
        service.
     *  Thus hook service is created and existing hook feature unit test is
        moved from lib/common/hook-spec.js to lib/services/hook-spec.js.
     *  Hook publish is added to publishExternalEvent method.

--- spec/lib/services/hook-spec.js
+++ spec/lib/services/hook-spec.js
@@ -5,32 +5,41 @@
 var nock = require('nock');
 
 describe('Hook', function () {
-    var hook, waterline, findStub, _publishSpy;
+    var hookService , waterline, findStub, _publishSpy;
     var hooks = [
         {url: 'http://172.1.1.1:8080/test', id: 'id1'},
         {url: 'https://172.1.1.0:8080/test', id: 'id2'}
     ];
     var payload = {"hello": "world", "routingKey": "test"};
     
-    before("Before hook test", function(){
+    helper.before();
+    
+    before("Before hookService  test", function(){
         helper.setupInjector([
-            helper.require('/lib/common/hook'),
+            helper.require('/lib/services/hook'),
             helper.require('/lib/common/http-tool'),
             helper.require('/lib/services/waterline')
         ]);
-        hook = helper.injector.get('Hook');
+        hookService = helper.injector.get('Services.Hook');
         waterline = helper.injector.get('Services.Waterline');
         waterline.hooks = {
             find: function() {}
         };
         findStub = sinon.stub(waterline.hooks, 'find');
-        _publishSpy = sinon.spy(hook, '_publish');
+        _publishSpy = sinon.spy(hookService , '_publish');
     });
 
-    afterEach("After hook test", function(){
+    helper.after();
+
+    afterEach("After hookService  test", function(){
         _publishSpy.reset();
         findStub.reset();
         nock.cleanAll();
+    });
+
+    it('should have start and stop methods', function(){
+        expect(hookService .start).to.be.a.method;
+        expect(hookService .stop).to.be.a.method;
     });
 
     it('should post data to hook url with http service', function (done) {
@@ -43,10 +52,10 @@ describe('Hook', function () {
             .post('/test')
             .reply(201, 'OK');
         findStub.withArgs({}).resolves([hooks[0]]);
-        return hook.publish(payload)
+        return hookService .publish(payload)
         .then(function(){
-            expect(hook._publish).to.have.been.calledOnce;
-            expect(hook._publish).to.have.been.calledWith(hooks[0], payload);
+            expect(hookService ._publish).to.have.been.calledOnce;
+            expect(hookService ._publish).to.have.been.calledWith(hooks[0], payload);
             expect(waterline.hooks.find).to.have.been.calledOnce;
             done();
         });
@@ -71,11 +80,11 @@ describe('Hook', function () {
             .reply(201, 'OK');
 
         findStub.withArgs({}).resolves(hooks);
-        return hook.publish(payload)
+        return hookService .publish(payload)
         .then(function(){
-            expect(hook._publish).to.have.been.calledTwice;
-            expect(hook._publish).to.have.been.calledWith(hooks[0], payload);
-            expect(hook._publish).to.have.been.calledWith(hooks[1], payload);
+            expect(hookService ._publish).to.have.been.calledTwice;
+            expect(hookService ._publish).to.have.been.calledWith(hooks[0], payload);
+            expect(hookService ._publish).to.have.been.calledWith(hooks[1], payload);
             expect(waterline.hooks.find).to.have.been.calledOnce;
             done();
         });
@@ -83,9 +92,9 @@ describe('Hook', function () {
 
     it('should not post data to hook url if no hook exists', function () {
         findStub.withArgs({}).resolves([]);
-        return hook.publish(payload)
+        return hookService .publish(payload)
         .then(function(){
-            expect(hook._publish).to.have.not.been.called;
+            expect(hookService ._publish).to.have.not.been.called;
         });
     });
 
@@ -100,16 +109,16 @@ describe('Hook', function () {
             .reply(201, 'OK');
         findStub.withArgs({}).resolves(hooks);
         _publishSpy.restore();
-        sinon.stub(hook, '_publish').withArgs(hooks[0], payload)
+        sinon.stub(hookService , '_publish').withArgs(hooks[0], payload)
             .rejects(new Error('Failed to send data to all hooks'));
-        return hook.publish(payload)
+        return hookService .publish(payload)
         .then(function(){
             done(new Error('Test should fail'));
         })
         .catch(function(err){
-            expect(hook._publish).to.have.been.calledTwice;
-            expect(hook._publish).to.have.been.calledWith(hooks[0], payload);
-            expect(hook._publish).to.have.been.calledWith(hooks[1], payload);
+            expect(hookService ._publish).to.have.been.calledTwice;
+            expect(hookService ._publish).to.have.been.calledWith(hooks[0], payload);
+            expect(hookService ._publish).to.have.been.calledWith(hooks[1], payload);
             expect(err.message).equals('Failed to send data to all hooks');
             expect(waterline.hooks.find).to.have.been.calledOnce;
             done();
