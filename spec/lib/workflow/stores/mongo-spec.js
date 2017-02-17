5ca288beaebfe1d095a424de25af57cd354967c1
yuchi-zhang <yuchi.zhang@emc.com>
2017-02-16 16:17:35 -0800

    cleared up merging error

--- spec/lib/workflow/stores/mongo-spec.js
+++ spec/lib/workflow/stores/mongo-spec.js
@@ -663,11 +663,7 @@ describe('Task Graph mongo store interface', function () {
         query.$or.push(dependenciesItr);
         
         delete dependenciesItr['dependencies.' + data.taskId];
-<<<<<<< HEAD
         dependenciesItr['dependencies.anyOf.' + data.taskId] = {
-=======
-        dependenciesItr['dependencies.either.' + data.taskId] = {
->>>>>>> 5fa4cb10050ae150a13431ed4b86be47d37da0f7
             $in: _.difference(Constants.Task.FinishedStates, [data.state])
         };
         query.$or.push(dependenciesItr);
